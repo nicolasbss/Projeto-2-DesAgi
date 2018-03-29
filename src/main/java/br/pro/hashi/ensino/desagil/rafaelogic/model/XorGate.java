eac43923c9b131b65af2f9da10fd208cdf320bae
elijose55 <elijose55@hotmail.com>
2018-03-29 20:02:55 -0300

    XorGate feito e testado

---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/XorGate.java
@@ -0,0 +1,39 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
+
+public class XorGate extends Gate {
+	
+	private NandGate nandLeft;
+	private NandGate nandBottom;
+	private NandGate nandRight;
+	private NandGate nandTop;
+	
+	public XorGate(){
+		nandLeft = new NandGate();
+		nandBottom = new NandGate();
+		nandRight = new NandGate();
+		nandTop = new NandGate();
+		
+		nandTop.connect(1, nandLeft);
+		nandBottom.connect(0, nandLeft);
+		nandRight.connect(0, nandTop);
+		nandRight.connect(1, nandBottom);
+	}
+	
+	@Override
+	public boolean read() {
+		return nandRight.read();
+	}
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		if (pinIndex == 0) {
+			nandTop.connect(0, emitter);
+			nandLeft.connect(0, emitter);
+
+		}
+		
+		if (pinIndex == 1) {
+			nandLeft.connect(1, emitter);
+			nandBottom.connect(1, emitter);
+		}
+	}
+}
