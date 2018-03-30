e836b00b39b9aee116c938a740dcf4485f5dd601
Nicolas <nicolasbs@al.insper.edu.br>
2018-03-29 22:45:36 -0300

    NotGate

---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/NotGate.java
@@ -0,0 +1,23 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
+
+public class NotGate extends Gate {
+	
+	private NandGate nandGate;
+	
+	
+	public NotGate() {
+		nandGate = new NandGate();
+	}
+	
+	@Override
+	public boolean read() {
+		return nandGate.read();
+	}
+	
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {		
+		nandGate.connect(0, emitter);
+		nandGate.connect(1, emitter);
+	}
+	
+}
