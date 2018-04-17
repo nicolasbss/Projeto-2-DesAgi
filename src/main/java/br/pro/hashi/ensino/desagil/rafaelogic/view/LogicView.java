c767f20a45472094fd73cd467b9872e5d0b334de
elijose55 <elijose55@hotmail.com>
2018-04-16 23:08:10 -0300

    codigo completo e testado

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/LogicView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/LogicView.java
@@ -1,15 +1,26 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.view;
 
 import java.awt.event.ActionEvent;
+
 import java.awt.event.ActionListener;
 import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JColorChooser;
 
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Gate;
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Source;
 
 
 
-public class LogicView extends JPanel implements ActionListener {
+public class LogicView extends SimplePanel implements ActionListener, MouseListener {
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -19,47 +30,49 @@ public class LogicView extends JPanel implements ActionListener {
 	// objetos da classe JCheckBox usada para criar os checkbox
     private JCheckBox in1Check;
     private JCheckBox in2Check;
-    private JCheckBox outCheck;
 
 	private Gate gate;
+	
+	private Color color;
+	private Image image;
+	private boolean led_on;
     
     
 	public LogicView(Gate gate) {
+		super(400,310);
 		this.gate = gate;
 		
 		//criacao dos objetos da classe JCheckBox usados para criar os checkbox
 		in1Check = new JCheckBox();
 		in2Check = new JCheckBox();
-		outCheck = new JCheckBox();
 		
-		JLabel inLabel = new JLabel("Entrada");
-		JLabel outLabel = new JLabel("Saida");
 		
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		
 		if (gate.getSize() == 1) {
-			add(inLabel);
-			add(in1Check);
+			add(in1Check, 40, 165, 17, 17);
 		}
 		if (gate.getSize() == 2) {
-			add(inLabel);
-			add(in1Check);
-			add(in2Check);
+			add(in1Check, 40, 127, 17, 17);
+			add(in2Check, 40, 205, 17, 17);
 			
 		}
-		
-		add(outLabel);
-		add(outCheck);
 		
 		//estabelecendo que o subpainel reage a mudancas nos dois primeiros checkboxes
 		in1Check.addActionListener(this);
 		in2Check.addActionListener(this);
 		
-		//estabelecendo que o terceiro checkbox nao pode ser modificado
-		outCheck.setEnabled(false);
+		color = Color.BLACK;
+		
+		String path = "/" + gate.toString() + ".png";
+		URL url = getClass().getResource(path);
+		image = new ImageIcon(url).getImage();
 
 		update();
+		
+
+		
+		addMouseListener(this);
 	}
 	
 	private void update() {
@@ -94,17 +107,65 @@ public class LogicView extends JPanel implements ActionListener {
 			
 		}
 		
-
-		boolean result;
-		result = gate.read();
+		
 		//devolvendo o estado da ultima checkbox de acordo com o resultado da porta logica
-		outCheck.setSelected(result);
+		if(gate.read() == false){
+			color = Color.BLACK;
+			led_on = false;
+			repaint();
+		}
+		if(gate.read() == true){
+			color = Color.RED;
+			led_on = true;
+			repaint();
+		}
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
 	}
+	
+	@Override
+	public void mouseClicked(MouseEvent event) {
+
+		int x = event.getX();
+		int y = event.getY();
+		//codigo para determinar se o clique foi dentro ou fora do circulo
+		double click = Math.sqrt(Math.pow(335-x, 2)+Math.pow(174-y, 2));
+		
+		if(click <= 10 && led_on == true) {
+			color = JColorChooser.showDialog(this, null, color);
+			repaint();
+		}
+	}
+	@Override
+	public void mousePressed(MouseEvent event) {
+	}
+	@Override
+	public void mouseReleased(MouseEvent event) {
+	}
+	@Override
+	public void mouseEntered(MouseEvent event) {
+	}
+	@Override
+	public void mouseExited(MouseEvent event) {
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+
+
+		g.drawImage(image, 43, 100, 300, 150, null);
+
+		// Desenha um circulo cheio
+		g.setColor(color);
+		g.fillOval(325, 164, 20, 20);
+
+		getToolkit().sync();
+    }
 		
 
 }
---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/SimplePanel.java
@@ -0,0 +1,26 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.view;
+
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class SimplePanel extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public SimplePanel(int width, int height) {
+		
+		setLayout(null);
+		setPreferredSize(new Dimension(width, height));
+	}
+	
+	public Component add(Component comp, int x, int y, int width, int height) {
+
+		super.add(comp);
+		comp.setBounds(x, y, width, height);
+		return comp;
+	}
+	
+
+}
